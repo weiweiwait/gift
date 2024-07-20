8fa0277ad1b34f8aea193f6fb0901e345b83b918
fansg666 <2680315178@qq.com>
2024-07-20 13:54:12 +0800

    grouting_test

--- groutine.go
+++ groutine.go
@@ -1,1 +1,25 @@
-package gift
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func parent() {
+	for i := 'a'; i < 'z'; i++ {
+		fmt.Printf("%d\n", i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+func child() {
+	for i := 'a'; i < 'z'; i++ {
+		fmt.Printf("%c\n", i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+func main() {
+	go parent()
+	go child()
+	time.Sleep(1500 * time.Millisecond)
+	fmt.Println("main")
+}
