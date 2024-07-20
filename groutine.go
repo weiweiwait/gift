d3a51fd8f63e17db7c394d79f0632c2a60252290
fansg666 <2680315178@qq.com>
2024-07-20 15:00:21 +0800

    线程开销

--- groutine.go
+++ groutine.go
@@ -17,7 +17,7 @@ func init() {
 }
 func parent() {
 	defer wg.Done() //所有执行完后执行
-	go child()
+	//go child()
 	for i := 'a'; i < 'z'; i++ {
 		fmt.Printf("%d\n", i)
 		//time.Sleep(500 * time.Millisecond)
@@ -30,22 +30,31 @@ func child() {
 		time.Sleep(500 * time.Millisecond)
 	}
 }
-func main1() { //runtime来调度协程，所以main结束了就全结束了
+func main() { //runtime来调度协程，所以main结束了就全结束了
 	//main是一个特殊的协程
-	//go parent()
-	go func(n int) {
-		defer wg.Done() //所有执行完后执行
-		go child()
-		for i := 'a'; i < 'z'; i++ {
-			fmt.Printf("%d\n", i)
-			//time.Sleep(500 * time.Millisecond)
-		}
-	}(4)
+	go parent()
+	//go func(n int) {
+	//	defer wg.Done() //所有执行完后执行
+	//	go child()
+	//	for i := 'a'; i < 'z'; i++ {
+	//		fmt.Printf("%d\n", i)
+	//		//time.Sleep(500 * time.Millisecond)
+	//	}
+	//}(4)
 	go child()
 	fmt.Println("main")
 	//time.Sleep(1500 * time.Millisecond)
 	wg.Wait() //等wg减为0
+	cpuN := runtime.NumCPU()
+	fmt.Println("逻辑核数", cpuN)
+	runtime.GOMAXPROCS(cpuN / 2) //限制go进程最多使用的核数
+	const P = 100000
+	for i := 0; i < P; i++ {
+		go time.Sleep(10 * time.Second)
+	}
+	fmt.Println("进程中存活的协程数", runtime.NumGoroutine())
 }
-func main() {
-	fmt.Println(runtime.NumCPU())
-}
+
+//func main() {
+//	fmt.Println(runtime.NumCPU())
+//}
