ec599072dddd36297c33b09b2aa25a50c1bba6d5
fansg666 <2680315178@qq.com>
2024-07-23 15:38:43 +0800

    清除全部订单记录

--- database/order.go
+++ database/order.go
@@ -24,3 +24,10 @@ func CreateOrder(userid, giftid int) int {
 		return order.Id
 	}
 }
+
+//清除全部订单记录
+
+func ClearOrders() error {
+	db := GetGiftDBConnection()
+	return db.Where("id>0").Delete(Order{}).Error
+}
